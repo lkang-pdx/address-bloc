149a361106c7899c4af6eea1eb70f4e285c02854
Luna Kang <luna.kang.ca@gmail.com>
2016-10-13 10:26:42 -0700

    A5 Models completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes an entry from the address book" do
+      book = AddressBook.new
+      book.add_entry("Luna Kang", "012.123.4561", "luna.kang@lovelace.com")
+
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email_address = "augusta.king@lovelace.com"
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq("Luna Kang")
+    end
+  end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
